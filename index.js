35187758de63750598e4ebd9754111b465c938a0
‘Michael <pickup.patterns@gmail.com>
2018-10-23 11:55:20 -0700

    modified content

--- index.js
+++ index.js
@@ -4,46 +4,54 @@
 //    console.log('hello world!, for the ', count, 'time');
 //    count++;
 //}
-//form functionality
-//function handleSubmit(event) {
-//    var form =event.target;
-//    var elements = form.elements;
 
-//    var name = elements.name.value;
-//    console.log('name', name);
-
-//   var comments = elements.comments.value;
-//    console.log('comments', comments);
-//}
-//animal guess game:
+//to multiline comment ou cmd + /
+//pizza form functionality:
 function handleSubmit(event) {
-        var form =event.target;
-        var elements = form.elements;
-    
-        var guess =  elements.guess.value;
-        
-        var result = document.getElementById('result');
-        
-        result.textContent = 'You made a guess of ' + guess;
+   var form = event.target;
+   var elements = form.elements;
+   var toppingchoices = elements.toppings;
 
-        var correct = 7;
-    //if equal -> correct!
-         if (guess == correct) {
-            result.textContent = 'Holy Cow, you did it!'
-         }
-    //else if -> too low
-         else if (guess < correct) {
-            result.textContent = 'Nope, too low' 
-         }
-         //else if -> too high
-    //else if -> waaaaaay too high
-        else if (guess > (correct*2)) {
-            result.textContent = 'You are like waaay off'  
-        }     
-        else if (guess > correct) {
-            result.textContent = 'Nope, too high' 
-        }
-    }
+   var selectedToppings= '';
+
+   for(var i=0; i < selectedToppings.length; i++) {
+       var toppingchoices = toppings[i];
+       if(toppings.checked) {
+           selectedTopping += '' + topping.value;
+       }
+
+   }
+console.log(selectedToppings);
+}
+//Animal Guessing Game
+// function handleSubmit(event) {
+//         var form =event.target;
+//         var elements = form.elements;
+    
+//         var guess =  elements.guess.value;
+        
+//         var result = document.getElementById('result');
+        
+//         result.textContent = 'You made a guess of ' + guess;
+
+//         var correct = 7;
+//     //if equal -> correct!
+//          if (guess == correct) {
+//             result.textContent = 'Holy Cow, you did it!'
+//          }
+//     //else if -> too low
+//          else if (guess < correct) {
+//             result.textContent = 'Nope, too low' 
+//          }
+//          //else if -> too high
+//     //else if -> waaaaaay too high
+//         else if (guess > (correct*2)) {
+//             result.textContent = 'You are like waaay off'  
+//         }     
+//         else if (guess > correct) {
+//             result.textContent = 'Nope, too high' 
+//         }
+//     }
 
         
     
